4b7911a9732264a3b1c70d43c95d7b869f1f2eb4
uprog-24 <tatrykova2002@yandex.ru>
2024-09-18 16:11:12 +0300

    fix receive dma

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -77,103 +77,109 @@ void SystemClock_Config(void);
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
-int main(void) {
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
 
-	/* USER CODE BEGIN 1 */
+  /* USER CODE BEGIN 1 */
 	uint8_t counter = 0;
-	/* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-	/* USER CODE END Init */
+  /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-	/* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_DMA_Init();
-	MX_TIM16_Init();
-	MX_TIM3_Init();
-	MX_USART1_UART_Init();
-	/* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_TIM16_Init();
+  MX_TIM3_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
 
 	memset(start_buff, '1', START_BUFF_SIZE / 4);
 	memset(start_buff + 256, '2', START_BUFF_SIZE / 4);
 	memset(start_buff + 512, '3', START_BUFF_SIZE / 4);
 	memset(start_buff + 768, '4', START_BUFF_SIZE / 4);
 
-	transmit_data_to_uart_dma(start_buff, START_BUFF_SIZE, tx_dma,
-	TX_DMA_SIZE);
+//	transmit_data_to_uart_dma(start_buff, START_BUFF_SIZE, tx_dma,
+//	TX_DMA_SIZE);
 
-	/* USER CODE END 2 */
 
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 	while (1) {
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
-		HAL_Delay(100);
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
-		HAL_Delay(100);
+//		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
+//		HAL_Delay(100);
+//		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
+//		HAL_Delay(100);
 
 		receive_data_from_uart_dma(rx_dma, RX_DMA_SIZE, final_buff,
 		FINAL_BUFF_SIZE);
 		counter++;
-		/* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-		/* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
 	}
 
-	/* USER CODE END 3 */
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
-void SystemClock_Config(void) {
-	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
-	RCC_PeriphCLKInitTypeDef PeriphClkInit = { 0 };
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
-	RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-		Error_Handler();
-	}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-			| RCC_CLOCKTYPE_PCLK1;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
-		Error_Handler();
-	}
-	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
-	PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
-	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
-		Error_Handler();
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /* USER CODE BEGIN 4 */
@@ -181,16 +187,17 @@ void SystemClock_Config(void) {
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
-void Error_Handler(void) {
-	/* USER CODE BEGIN Error_Handler_Debug */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1) {
 	}
-	/* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
--- Core/Src/usart.c
+++ Core/Src/usart.c
@@ -63,121 +63,129 @@ DMA_HandleTypeDef hdma_usart1_tx;
 
 /* USART1 init function */
 
-void MX_USART1_UART_Init(void) {
+void MX_USART1_UART_Init(void)
+{
 
-	/* USER CODE BEGIN USART1_Init 0 */
+  /* USER CODE BEGIN USART1_Init 0 */
 
-	/* USER CODE END USART1_Init 0 */
+  /* USER CODE END USART1_Init 0 */
 
-	/* USER CODE BEGIN USART1_Init 1 */
+  /* USER CODE BEGIN USART1_Init 1 */
 
-	/* USER CODE END USART1_Init 1 */
-	huart1.Instance = USART1;
-	huart1.Init.BaudRate = 9600;
-	huart1.Init.WordLength = UART_WORDLENGTH_8B;
-	huart1.Init.StopBits = UART_STOPBITS_1;
-	huart1.Init.Parity = UART_PARITY_NONE;
-	huart1.Init.Mode = UART_MODE_TX_RX;
-	huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-	huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-	huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-	huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-	if (HAL_UART_Init(&huart1) != HAL_OK) {
-		Error_Handler();
-	}
-	/* USER CODE BEGIN USART1_Init 2 */
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
 
-	/* USER CODE END USART1_Init 2 */
+  /* USER CODE END USART1_Init 2 */
 
 }
 
-void HAL_UART_MspInit(UART_HandleTypeDef *uartHandle) {
+void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
+{
 
-	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
-	if (uartHandle->Instance == USART1) {
-		/* USER CODE BEGIN USART1_MspInit 0 */
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(uartHandle->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspInit 0 */
 
-		/* USER CODE END USART1_MspInit 0 */
-		/* USART1 clock enable */
-		__HAL_RCC_USART1_CLK_ENABLE();
+  /* USER CODE END USART1_MspInit 0 */
+    /* USART1 clock enable */
+    __HAL_RCC_USART1_CLK_ENABLE();
 
-		__HAL_RCC_GPIOA_CLK_ENABLE();
-		/**USART1 GPIO Configuration
-		 PA9     ------> USART1_TX
-		 PA10     ------> USART1_RX
-		 */
-		GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_10;
-		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-		GPIO_InitStruct.Pull = GPIO_NOPULL;
-		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-		GPIO_InitStruct.Alternate = GPIO_AF1_USART1;
-		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**USART1 GPIO Configuration
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF1_USART1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-		/* USART1 DMA Init */
-		/* USART1_RX Init */
-		hdma_usart1_rx.Instance = DMA1_Channel3;
-		hdma_usart1_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
-		hdma_usart1_rx.Init.PeriphInc = DMA_PINC_DISABLE;
-		hdma_usart1_rx.Init.MemInc = DMA_MINC_ENABLE;
-		hdma_usart1_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-		hdma_usart1_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-		hdma_usart1_rx.Init.Mode = DMA_CIRCULAR;
-		hdma_usart1_rx.Init.Priority = DMA_PRIORITY_LOW;
-		if (HAL_DMA_Init(&hdma_usart1_rx) != HAL_OK) {
-			Error_Handler();
-		}
+    /* USART1 DMA Init */
+    /* USART1_RX Init */
+    hdma_usart1_rx.Instance = DMA1_Channel3;
+    hdma_usart1_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_usart1_rx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_usart1_rx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_usart1_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_usart1_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_usart1_rx.Init.Mode = DMA_NORMAL;
+    hdma_usart1_rx.Init.Priority = DMA_PRIORITY_LOW;
+    if (HAL_DMA_Init(&hdma_usart1_rx) != HAL_OK)
+    {
+      Error_Handler();
+    }
 
-		__HAL_LINKDMA(uartHandle, hdmarx, hdma_usart1_rx);
+    __HAL_LINKDMA(uartHandle,hdmarx,hdma_usart1_rx);
 
-		/* USART1_TX Init */
-		hdma_usart1_tx.Instance = DMA1_Channel2;
-		hdma_usart1_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
-		hdma_usart1_tx.Init.PeriphInc = DMA_PINC_DISABLE;
-		hdma_usart1_tx.Init.MemInc = DMA_MINC_ENABLE;
-		hdma_usart1_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-		hdma_usart1_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-		hdma_usart1_tx.Init.Mode = DMA_CIRCULAR;
-		hdma_usart1_tx.Init.Priority = DMA_PRIORITY_LOW;
-		if (HAL_DMA_Init(&hdma_usart1_tx) != HAL_OK) {
-			Error_Handler();
-		}
+    /* USART1_TX Init */
+    hdma_usart1_tx.Instance = DMA1_Channel2;
+    hdma_usart1_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+    hdma_usart1_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_usart1_tx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_usart1_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_usart1_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_usart1_tx.Init.Mode = DMA_NORMAL;
+    hdma_usart1_tx.Init.Priority = DMA_PRIORITY_LOW;
+    if (HAL_DMA_Init(&hdma_usart1_tx) != HAL_OK)
+    {
+      Error_Handler();
+    }
 
-		__HAL_LINKDMA(uartHandle, hdmatx, hdma_usart1_tx);
+    __HAL_LINKDMA(uartHandle,hdmatx,hdma_usart1_tx);
 
-		/* USART1 interrupt Init */
-		HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
-		HAL_NVIC_EnableIRQ(USART1_IRQn);
-		/* USER CODE BEGIN USART1_MspInit 1 */
+    /* USART1 interrupt Init */
+    HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(USART1_IRQn);
+  /* USER CODE BEGIN USART1_MspInit 1 */
 
-		/* USER CODE END USART1_MspInit 1 */
-	}
+  /* USER CODE END USART1_MspInit 1 */
+  }
 }
 
-void HAL_UART_MspDeInit(UART_HandleTypeDef *uartHandle) {
+void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
+{
 
-	if (uartHandle->Instance == USART1) {
-		/* USER CODE BEGIN USART1_MspDeInit 0 */
+  if(uartHandle->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspDeInit 0 */
 
-		/* USER CODE END USART1_MspDeInit 0 */
-		/* Peripheral clock disable */
-		__HAL_RCC_USART1_CLK_DISABLE();
+  /* USER CODE END USART1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART1_CLK_DISABLE();
 
-		/**USART1 GPIO Configuration
-		 PA9     ------> USART1_TX
-		 PA10     ------> USART1_RX
-		 */
-		HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9 | GPIO_PIN_10);
+    /**USART1 GPIO Configuration
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9|GPIO_PIN_10);
 
-		/* USART1 DMA DeInit */
-		HAL_DMA_DeInit(uartHandle->hdmarx);
-		HAL_DMA_DeInit(uartHandle->hdmatx);
+    /* USART1 DMA DeInit */
+    HAL_DMA_DeInit(uartHandle->hdmarx);
+    HAL_DMA_DeInit(uartHandle->hdmatx);
 
-		/* USART1 interrupt Deinit */
-		HAL_NVIC_DisableIRQ(USART1_IRQn);
-		/* USER CODE BEGIN USART1_MspDeInit 1 */
+    /* USART1 interrupt Deinit */
+    HAL_NVIC_DisableIRQ(USART1_IRQn);
+  /* USER CODE BEGIN USART1_MspDeInit 1 */
 
-		/* USER CODE END USART1_MspDeInit 1 */
-	}
+  /* USER CODE END USART1_MspDeInit 1 */
+  }
 }
 
 /* USER CODE BEGIN 1 */
@@ -212,7 +220,6 @@ void receive_data_from_uart_dma(char *rx_buff, uint16_t rx_buff_len,
 		char *final_buff, uint16_t final_buff_len) {
 
 	uint16_t rx_half_len = rx_buff_len / 2;
-	index_byte = 0;
 
 	HAL_UART_Receive_DMA(&huart1, (uint8_t*) rx_buff, rx_buff_len);
 
@@ -232,6 +239,11 @@ void receive_data_from_uart_dma(char *rx_buff, uint16_t rx_buff_len,
 		index_byte += rx_half_len;
 	}
 
+		if (index_byte == final_buff_len) {
+			index_byte = 0;
+			memset(final_buff, '\0', final_buff_len);
+		}
+
 }
 
 uint32_t index_tx_byte = 0;
@@ -246,13 +258,11 @@ void transmit_data_to_uart_dma(char *start_buff, uint16_t start_buff_len,
 		HAL_UART_Transmit_DMA(&huart1, (uint8_t*) tx_buff, tx_buff_len);
 		while (!is_tx_full_completed) {
 		}
-		HAL_UART_DMAStop(&huart1);
 		index_tx_byte += tx_buff_len;
 	}
 
 	if (index_tx_byte == start_buff_len) {
 		index_tx_byte = 0;
-		HAL_UART_DMAStop(&huart1);
 		memset(tx_buff, '\0', tx_buff_len);
 	}
 }
